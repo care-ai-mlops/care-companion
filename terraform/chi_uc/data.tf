156af95fe5b6ad50d23c11837c673c2ac4bbe284
Yash Patel <60495318+YashPatel166@users.noreply.github.com>
2025-05-06 16:45:12 -0400

    Update data.tf

--- terraform/chi_uc/data.tf
+++ terraform/chi_uc/data.tf
@@ -14,23 +14,4 @@ data "openstack_networking_secgroup_v2" "allow_30000_32767" {
   name = "nodeport-30000-32767"
 }
 
-#data "openstack_networking_secgroup_v2" "allow_8000" {
-#  name = "allow-8000"
-#}
-#
-#data "openstack_networking_secgroup_v2" "allow_8080" {
-#  name = "allow-8080"
-#}
-#
-#data "openstack_networking_secgroup_v2" "allow_8081" {
-#  name = "allow-8081"
-#}
-#
-#data "openstack_networking_secgroup_v2" "allow_http_80" {
-#  name = "allow-http-80"
-#}
-#
-#data "openstack_networking_secgroup_v2" "allow_9090" {
-#  name = "allow-9090"
-#}
 
