a20b189dd4afac6f03615f78f6d02758dd60f0d8
YashPatel166 <yashspatel16@gmail.com>
2025-05-11 10:19:11 -0400

    platform files updated

---
+++ set-openstack-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# OpenStack Authentication
+export OS_AUTH_URL=https://openstack.tacc.chameleoncloud.org:5000/v3
+export OS_USERNAME="your_username"
+export OS_PASSWORD="your_password"
+export OS_PROJECT_NAME="your_project_name"
+export OS_USER_DOMAIN_NAME="chameleon"
+export OS_PROJECT_DOMAIN_NAME="chameleon"
+export OS_REGION_NAME="CHI@UC"
+export OS_INTERFACE=public
+export OS_IDENTITY_API_VERSION=3 
